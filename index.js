6cdab3050f56d92eafa0d36a2eadafaac5e2942d
NF RIDOY <nfridoy@gmail.com>
2023-08-20 06:16:25 +0600

    apply and Make Purchase btn disabled controled

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-let TotalPrice = 0;
+let totalPrice = 0;
 const productList_Element = document.getElementById("product-list");
+const totalPrice_Element = document.getElementById("total-price");
+const applyBtn_Element = document.getElementById("apply-Btn");
+const makePurchaseBtn_Element = document.getElementById("Make-Purchase");
 
-function getNumber(text) {
+
+function getPrice(text) {
     return parseFloat(text.children[1].children[2].children[0].innerText);
 }
 
@@ -9,13 +13,35 @@ function getProductName(target) {
     return target.children[1].children[1].innerText;
 }
 
-function buy(target) {  
-    let ProductPrice = getNumber(target);
-    // console.log(ProductPrice);
+function totalPriceUpdate(target) {
+    let productPrice = getPrice(target);
+    totalPrice += productPrice;
+    // console.log(totalPrice);
+
+    totalPrice_Element.innerText = totalPrice;
+    // return totalPrice;
+}
+
+function addProduct(target) {
     let ProductName = getProductName(target);
-    console.log(ProductName);
+    // console.log(ProductName);
 
     const newProduct = document.createElement("li");
     newProduct.innerText = ProductName;
-    productList_Element.appendChild(newProduct);    
+    productList_Element.appendChild(newProduct);
+}
+
+function buy(target) {
+    totalPriceUpdate(target);
+    addProduct(target);
+
+    if(totalPrice > 0) {
+        makePurchaseBtn_Element.removeAttribute("disabled");
+    }
+
+    if (totalPrice > 200) {
+        // alert("200");
+        // applyBtn_Element.setAttribute("disabled","none");
+        applyBtn_Element.removeAttribute("disabled");
+    }
 }
